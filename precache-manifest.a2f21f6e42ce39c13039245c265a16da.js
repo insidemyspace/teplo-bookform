2593a099a3443f50241c67906eb2a315c4e8452b
Anton <job@insidemy.space>
2020-02-23 03:42:11 +0300

    Updates

---
+++ precache-manifest.a2f21f6e42ce39c13039245c265a16da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650e1a4ce88cdf1ca6929fc6e589005e",
+    "url": "/teplo-bookform/index.html"
+  },
+  {
+    "revision": "d4ee11c1636d91864003",
+    "url": "/teplo-bookform/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "327922f2f5befa48f9fc",
+    "url": "/teplo-bookform/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d4ee11c1636d91864003",
+    "url": "/teplo-bookform/static/js/2.15be8404.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/teplo-bookform/static/js/2.15be8404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327922f2f5befa48f9fc",
+    "url": "/teplo-bookform/static/js/main.f763a1e3.chunk.js"
+  },
+  {
+    "revision": "3c6bb8b3a655de7c75a0",
+    "url": "/teplo-bookform/static/js/runtime-main.73631e53.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/teplo-bookform/static/media/persik.4e1ec840.png"
+  }
+]);
